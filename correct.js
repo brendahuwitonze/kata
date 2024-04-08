26ddca758cc023c79a3bbd9db151ba95fb92321b
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-04-08 12:51:21 +0200

    worked with a loop

--- correct.js
+++ correct.js
@@ -1,8 +1,20 @@
-function correct(string){
+function corrected(string){
     return string.replace(/0/g,'O')
                .replace(/1/g,"I")
                .replace(/5/g,"S")
              
    
 }
+// using it using for loop
+
+function correct(s){
+    s =s.split('')
+    for (let i=0;i<s.length;i++){
+        if(s[i]==='0')s[i]='O'
+        else if(s[i]==="1")s[i]='I'
+        else if(s[i]==="5")s[i]='S'  }
+    return s.join("")
+}
+
+console.log(correct("PAR15"))
 
--- sumofpositive.js
+++ sumofpositive.js
@@ -63,4 +63,3 @@
 // }
 // console.log(factorial(3))
 
-
