115d5a35af695dfdecc527643963b58cb6ef8ac4
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-04-11 03:00:26 +0200

    finding the min and max

---
+++ highAndLow.js
@@ -0,0 +1,15 @@
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+function numbers(n){
+var array = n.split("").map(Number);
+let highest =array[0];
+let  lowest =array[0];
+for (let i=1;i<array.length;i++){
+    if(array[i]>highest) highest=array[i]
+    if(array[i]<lowest)lowest=array[i]
+}
+return highest + " "+ lowest
+}
+console.log(numbers(("1 2 3 4 5")))
