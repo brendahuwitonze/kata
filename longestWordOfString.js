a4c2dab92ab86fff16e22166035f0b45bcb06760
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-06-24 15:05:16 +0200

    longest string in the string

--- longestWordOfString.js
+++ longestWordOfString.js
@@ -1,28 +1,22 @@
-// function longestWord(stringOfWords) {
-//     let strings = stringOfWords.split(" ")
-//     let longestWord = ''
-//     let longestLength =0
-//     for(const string of strings){
-//         string.length >longestLength?longestWord = string : 0
-//     }
-//     return longestWord;
-//   }
-//   console.log(longestWord('red blue grey'))
+
+function longestWord(stringOfWords) {
+  let strings = stringOfWords.split(" ")
+  console.log(strings)
+     let longestWord = strings[0]
+     for(let string of strings){
+        longestWord = string.length >=longestWord.length?string : longestWord
+     }
+     return longestWord;
+   }
 
 
+   //Refactor
 
-  function longestWord(stringOfWords) {
-    let newWord = stringOfWords.split(" ");
-  let longestWords = newWord[0]
-  for(word of newWord){
-      if(word.length>longestWord.length){
-         longestWords = word
-        
-      }
-  }
-  return  longestWords
-  }
-  console.log(longestWord("brendah kami usanaseBrendah"))
+   function longestWord(stringOfWords) {
+    return stringOfWords.split(" ").reduceRight((a,b)=>b.length>a.length?b:a)
+     
+     
+   }
 
 
 
